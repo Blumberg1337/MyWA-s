dcce08785f31b5692acf9807b2d016665c90272d
Blumberg1337 <nick.blumberg@outlook.de>
2022-05-23 03:20:44 +0200

    add load advice and custom options

---
+++ TrinketsTimer/6_Load/Load_Config.lua
@@ -0,0 +1,6 @@
+-- for my understanding it's best to load weakauras for a specific ingame character
+-- this way, you can fit this weakaura into your custom character's weakaura if you have one
+-- ...like only mrogue related stuff showing blade flurry cd, slice'n'dice uptime, etc.
+-- so my advice is to copy this weakaura per character and set your character's name here accordingly
+
+playerNameRealm = "<your character's name and realm>"
