f99e4dcb793ebce62930ca56a948d9f9b4d244f9
Blumberg1337 <nick.blumberg@outlook.de>
2022-06-06 04:32:37 +0200

    fix access to functions

--- TrinketsTimer/4_Actions/OnInit.lua
+++ TrinketsTimer/4_Actions/OnInit.lua
@@ -15,11 +15,11 @@ aura_env.compareTrinketTimes = function()
   local duration_1, end_1, duration_2, end_2
 
   -- we want to check for the trinkets aura uptimes first
-  duration_1, duration_2, end_1, end_2 = getTrinketAurasTime()
+  duration_1, duration_2, end_1, end_2 = aura_env.getTrinketAurasTime()
 
   -- if we didn't find any trinket aura, we check for the trinkets cds
   if (end_1 == -1 and end_2 == -1) then
-    duration_1, duration_2, end_1, end_2 = getTrinketCds()
+    duration_1, duration_2, end_1, end_2 = aura_env.getTrinketCds()
     -- set toggles for cd ready event of a trinket
     aura_env.cdReady_1 = false
     aura_env.cdReady_2 = false
@@ -31,7 +31,7 @@ aura_env.compareTrinketTimes = function()
     if (end_1 == 0 or end_2 == 0) then
       aura_env.cdReady_1 = end_1 == 0
       aura_env.cdReady_2 = end_2 == 0
-      glowing(false)
+      aura_env.glowing(false)
     end
 
     -- to show trinket cooldowns shortly before expiring, we swap the expirationTimes
@@ -51,18 +51,18 @@ aura_env.compareTrinketTimes = function()
         if (lastSeconds_1 <= aura_env.config.lastSeconds and lastSeconds_1 > 0) then
           end_2 = end_1 + 1
           aura_env.overwroteExpirationTime = true
-          glowing(true, aura_env.config.glowBuffsOnly)
+          aura_env.glowing(true, aura_env.config.glowBuffsOnly)
         end
       end
       if (lastSeconds_2 <= lastSeconds_1 or end_1 == 0) then
         if (lastSeconds_2 <= aura_env.config.lastSeconds and lastSeconds_2 > 0) then
           end_1 = end_2 + 1
           aura_env.overwroteExpirationTime = true
-          glowing(true, aura_env.config.glowBuffsOnly)
+          aura_env.glowing(true, aura_env.config.glowBuffsOnly)
         end
       end
       if (lastSeconds_1 > 5 and lastSeconds_2 > 5) then
-        glowing(false)
+        aura_env.glowing(false)
       end
     end
   end
@@ -89,7 +89,7 @@ aura_env.compareTrinketTimes = function()
 end
 
 -- function to present information about the active aura for either trinket with the shorter remaining uptime
-local getTrinketAurasTime = function()
+aura_env.getTrinketAurasTime = function()
   local end_1, end_2
   -- we check for the aura by its name from GetItemSpell(itemId)
   local aura_1 = AuraUtil.FindAuraByName(aura_env.trinket_1_spellName, "player")
@@ -98,7 +98,7 @@ local getTrinketAurasTime = function()
   -- set toggle for activeTrinket and glow and send glow event when there is at least one active trinket aura to true
   if (aura_1 or aura_2) then
     aura_env.activeTrinket = true
-    glowing(true)
+    aura_env.glowing(true)
   end
 
   -- if we found an aura from the use of either trinket get its duration, otherwise set it to 0
@@ -125,7 +125,7 @@ local getTrinketAurasTime = function()
 end
 
 -- function to present information about the cooldown of either trinket with the shorter remaining uptime
-local getTrinketCds = function()
+aura_env.getTrinketCds = function()
   local start_1, duration_1, end_1, start_2, duration_2, end_2
 
   -- set toggle for activeTrinket when there are no active trinket auras to false
@@ -175,7 +175,7 @@ local getTrinketCds = function()
   return duration_1, duration_2, end_1, end_2
 end
 
-local glowing = function(glow, restricted)
+aura_env.glowing = function(glow, restricted)
   if (aura_env.config.glow and not restricted) then
     aura_env.glow = glow == true
     WeakAuras.ScanEvents("TRINKET_TIMERS_GLOW")
