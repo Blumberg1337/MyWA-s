80f23956e8115dcd421ea3f8a83de3adc9ec9184
Blumberg1337 <nick.blumberg@outlook.de>
2022-07-12 00:18:43 +0200

    fix outfight event

--- ResistanceTracker/2_Trigger/Trigger1.lua
+++ ResistanceTracker/2_Trigger/Trigger1.lua
@@ -1,8 +1,8 @@
 -- trigger data (copy text inside of quotes)
 type = "Custom"
 eventType = "Event"
---PLAYER_REGEN_DISABLED = getting outfight event
-events = "PLAYER_REGEN_DISABLED, LOADING_SCREEN_DISABLED, UNIT_INVENTORY_CHANGED:player"
+--PLAYER_REGEN_ENABLED = getting outfight event
+events = "PLAYER_REGEN_ENABLED, LOADING_SCREEN_DISABLED, UNIT_INVENTORY_CHANGED:player"
 
 customTrigger = function()
   return aura_env.resistanceBuffsValue()
