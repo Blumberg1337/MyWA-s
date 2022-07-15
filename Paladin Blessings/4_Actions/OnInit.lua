bf9311006c56b75ceba0f898e7cd455ef0aca30d
Blumberg1337 <nick.blumberg@outlook.de>
2022-07-15 04:22:00 +0200

    simplify logic

--- Paladin Blessings/4_Actions/OnInit.lua
+++ Paladin Blessings/4_Actions/OnInit.lua
@@ -26,28 +26,28 @@ aura_env.kingsSpellIds = {20217, 25898}
 aura_env.sanctuarySpellIds = {27168, 27169, 20911, 20912, 20913, 20914, 25899}
 
 -- Data Model
--- [Class-Role] = {{paladinBlessings}, classFileName, talentTreeNumber, talentNumber}
+-- {Class-SpecRole, {paladinBlessings}, classFileName, talentTreeNumber, talentNumber}
 aura_env.defaultBuffPriorities = {
-  ["WARRIOR-MELEE"] = {{"SALVATION", "MIGHT", "KINGS", "LIGHT", "SANCTUARY"}, "WARRIOR", 2, 13 --[[Sweeping Strikes]]},
-  ["WARRIOR-TANK"] = {{"KINGS", "MIGHT", "LIGHT", "SANCTUARY"}, "WARRIOR", 3, 19 --[[Shield Slam]]},
-  ["PALADIN-HEAL"] = {{"KINGS", "Wisdom", "SALVATION", "LIGHT", "SANCTUARY"}, "PALADIN", 1, 17 --[[Holy Shock]]},
-  ["PALADIN-TANK"] = {{"KINGS", "SANCTUARY", "WISDOM", "LIGHT", "MIGHT"}, "PALADIN", 2, 19 --[[Holy Shield]]},
-  ["PALADIN-MELEE"] = {{"SALVATION", "MIGHT", "KINGS", "WISDOM", "LIGHT", "SANCTUARY"}, "PALADIN", 3, 22 --[[Crusader Strike]]},
-  ["HUNTER-BM"] = {{"MIGHT", "KINGS", "SALVATION", "WISDOM", "LIGHT", "SANCTUARY"}, "HUNTER", 1, 21 --[[The Beast Within]]},
-  ["HUNTER-SV"] = {{"KINGS", "MIGHT", "SALVATION", "WISDOM", "LIGHT", "SANCTUARY"}, "HUNTER", 3, 21 --[[Expose Weakness]]},
-  ["ROGUE"] = {{"SALVATION", "MIGHT", "KINGS", "LIGHT", "SANCTUARY"}, "ROGUE"},
-  ["PRIEST-HEAL"] = {{"KINGS", "WISDOM", "SALVATION", "LIGHT", "SANCTUARY"}, "PRIEST", 2, 3 --[[Holy Specialization]]},
-  ["PRIEST-CASTER"] = {{"SALVATION", "KINGS", "WISDOM", "LIGHT", "SANCTUARY"}, "PRIEST", 3, 19 --[[Shadowform]]},
-  ["SHAMAN-CASTER"] = {{"SALVATION", "KINGS", "WISDOM", "LIGHT", "SANCTUARY"}, "SHAMAN", 1, 17 --[[Elemental Mastery]]},
-  ["SHAMAN-MELEE"] = {{"SALVATION", "MIGHT", "KINGS", "WISDOM", "LIGHT", "SANCTUARY"}, "SHAMAN", 2, 18 --[[Dual Wield]]},
-  ["SHAMAN-HEAL"] = {{"KINGS", "WISDOM", "SALVATION", "LIGHT", "SANCTUARY"}, "SHAMAN", 3, 20 --[[Earth Shield]]},
-  ["MAGE"] = {{"WISDOM", "KINGS", "SALVATION", "LIGHT", "SANCTUARY"}, "MAGE"},
-  ["WARLOCK"] = {{"SALVATION", "KINGS", "WISDOM", "LIGHT", "SANCTUARY"}, "WARLOCK"},
-  ["DRUID-CASTER"] = {{"SALVATION", "KINGS", "WISDOM", "LIGHT", "SANCTUARY"}, "DRUID", 1, 18 --[[Moonkin Form]]},
-  ["DRUID-TANK"] = {{"KINGS", "MIGHT", "LIGHT", "SANCTUARY", "WISDOM"}, "DRUID", 2, 21 --[[Mangle]]},
-  ["DRUID-MELEE"] = {{"SALVATION", "MIGHT", "KINGS", "WISDOM", "LIGHT", "SANCTUARY"}, "DRUID", 2, 21 --[[Mangle]]},
-  ["DRUID-HEAL"] = {{"KINGS", "WISDOM", "SALVATION", "LIGHT", "SANCTUARY"}, "DRUID", 3, 14 --[[Empowered Touch]]},
-  ["PET"] = {{"MIGHT", "KINGS", "SANCTUARY", "LIGHT", "SALVATION"}},
+  {"WARRIOR-MELEE", {"SALVATION", "MIGHT", "KINGS", "LIGHT", "SANCTUARY"}, "WARRIOR", 2, 13 --[[Sweeping Strikes]]},
+  {"WARRIOR-TANK", {"KINGS", "MIGHT", "LIGHT", "SANCTUARY"}, "WARRIOR", 3, 19 --[[Shield Slam]]},
+  {"PALADIN-HEAL", {"KINGS", "Wisdom", "SALVATION", "LIGHT", "SANCTUARY"}, "PALADIN", 1, 17 --[[Holy Shock]]},
+  {"PALADIN-TANK", {"KINGS", "SANCTUARY", "WISDOM", "LIGHT", "MIGHT"}, "PALADIN", 2, 19 --[[Holy Shield]]},
+  {"PALADIN-MELEE", {"SALVATION", "MIGHT", "KINGS", "WISDOM", "LIGHT", "SANCTUARY"}, "PALADIN", 3, 22 --[[Crusader Strike]]},
+  {"HUNTER-BM", {"MIGHT", "KINGS", "SALVATION", "WISDOM", "LIGHT", "SANCTUARY"}, "HUNTER", 1, 21 --[[The Beast Within]]},
+  {"HUNTER-SV", {"KINGS", "MIGHT", "SALVATION", "WISDOM", "LIGHT", "SANCTUARY"}, "HUNTER", 3, 21 --[[Expose Weakness]]},
+  {"ROGUE", {"SALVATION", "MIGHT", "KINGS", "LIGHT", "SANCTUARY"}, "ROGUE"},
+  {"PRIEST-HEAL", {"KINGS", "WISDOM", "SALVATION", "LIGHT", "SANCTUARY"}, "PRIEST", 2, 3 --[[Holy Specialization]]},
+  {"PRIEST-CASTER", {"SALVATION", "KINGS", "WISDOM", "LIGHT", "SANCTUARY"}, "PRIEST", 3, 19 --[[Shadowform]]},
+  {"SHAMAN-CASTER", {"SALVATION", "KINGS", "WISDOM", "LIGHT", "SANCTUARY"}, "SHAMAN", 1, 17 --[[Elemental Mastery]]},
+  {"SHAMAN-MELEE", {"SALVATION", "MIGHT", "KINGS", "WISDOM", "LIGHT", "SANCTUARY"}, "SHAMAN", 2, 18 --[[Dual Wield]]},
+  {"SHAMAN-HEAL", {"KINGS", "WISDOM", "SALVATION", "LIGHT", "SANCTUARY"}, "SHAMAN", 3, 20 --[[Earth Shield]]},
+  {"MAGE", {"WISDOM", "KINGS", "SALVATION", "LIGHT", "SANCTUARY"}, "MAGE"},
+  {"WARLOCK", {"SALVATION", "KINGS", "WISDOM", "LIGHT", "SANCTUARY"}, "WARLOCK"},
+  {"DRUID-CASTER", {"SALVATION", "KINGS", "WISDOM", "LIGHT", "SANCTUARY"}, "DRUID", 1, 18 --[[Moonkin Form]]},
+  {"DRUID-TANK", {"KINGS", "MIGHT", "LIGHT", "SANCTUARY", "WISDOM"}, "DRUID", 2, 21 --[[Mangle]]},
+  {"DRUID-MELEE", {"SALVATION", "MIGHT", "KINGS", "WISDOM", "LIGHT", "SANCTUARY"}, "DRUID", 2, 21 --[[Mangle]]},
+  {"DRUID-HEAL", {"KINGS", "WISDOM", "SALVATION", "LIGHT", "SANCTUARY"}, "DRUID", 3, 14 --[[Empowered Touch]]},
+  {"PET", {"MIGHT", "KINGS", "SANCTUARY", "LIGHT", "SALVATION"}},
 }
 
 aura_env.iterateGroupMembersFor = function(reason)
@@ -116,57 +116,36 @@ aura_env.sendPaladinBlessingEvents = function()
   local classFilename = UnitClassBase("player")
   local customEvent = "PALADIN_BLESSING_PRIORITY_"
   local sanctuaryPriority, kingsPriority = nil
-  if (classFilename == "WARRIOR") then
-  end
-  if (classFilename == "HUNTER") then
-    -- Check for Expose Weakness -> SV
-    local _, _, _, _, rank = GetTalentInfo(3,21)
-    if (rank > 0) then
-      for i=1, i <= aura_env.paladinCount do
-        if (sanctuaryAndKingsAvailability) then
-          if (aura_env.defaultBuffPriorities["HUNTER-SV"][i] == "SANCTUARY" or
-              aura_env.defaultBuffPriorities["HUNTER-SV"][i] == "KINGS") then
-            WeakAuras.ScanEvent(customEvent..aura_env.defaultBuffPriorities["HUNTER-SV"][i])
-          end
-        elseif (kingsAvailability or sanctuaryAvailability) then
-          if (kingsAvailability and aura_env.defaultBuffPriorities["HUNTER-SV"][i] == "KINGS") then
-            kingsPriority = i
-            if (sanctuaryPriority and kingsPriority < sanctuaryPriority) then
-              WeakAuras.ScanEvent(customEvent..aura_env.defaultBuffPriorities["HUNTER-SV"][i])
+
+  for i=1, #aura_env.defaultBuffPriorities do
+    if (classFileName == aura_env.defaultBuffPriorities[i][3]) then
+      local talentTreeNumber = aura_env.defaultBuffPriorities[i][4]
+      local talentNumber = aura_env.defaultBuffPriorities[i][5]
+      local _, _, _, _, rank = GetTalentInfo(talentTreeNumber, talentNumber)
+      
+      if (rank > 0) then
+        for k=1, k <= aura_env.paladinCount do
+          if (sanctuaryAndKingsAvailability) then
+            if (aura_env.defaultBuffPriorities[i][2][k] == "SANCTUARY" or
+                aura_env.defaultBuffPriorities[i][2][k] == "KINGS") then
+              WeakAuras.ScanEvent(customEvent..aura_env.defaultBuffPriorities[i][2][k])
             end
-          end
-          if (sanctuaryAvailability and aura_env.defaultBuffPriorities["HUNTER-SV"][i] == "SANCTUARY") then
-            sanctuaryPriority = i
-            if (kingsPriority and sanctuaryPriority < kingsPriority) then
-              WeakAuras.ScanEvent(customEvent..aura_env.defaultBuffPriorities["HUNTER-SV"][i])
+          elseif (kingsAvailability or sanctuaryAvailability) then
+            if (kingsAvailability and aura_env.defaultBuffPriorities[i][2][k] == "KINGS") then
+              kingsPriority = i
+              if (sanctuaryPriority and kingsPriority < sanctuaryPriority) then
+                WeakAuras.ScanEvent(customEvent..aura_env.defaultBuffPriorities[i][2][k])
+              end
             end
-          end
-        else
-          WeakAuras.ScanEvent(customEvent..aura_env.defaultBuffPriorities["HUNTER-SV"][i])
-        end
-      end
-    else -- -> BM
-      for i=1, i <= aura_env.paladinCount do
-        if (sanctuaryAndKingsAvailability) then
-          if (aura_env.defaultBuffPriorities["HUNTER-BM"][i] == "SANCTUARY" or
-              aura_env.defaultBuffPriorities["HUNTER-BM"][i] == "KINGS") then
-            WeakAuras.ScanEvent(customEvent..aura_env.defaultBuffPriorities["HUNTER-BM"][i])
-          end
-        elseif (kingsAvailability or sanctuaryAvailability) then
-          if (kingsAvailability and aura_env.defaultBuffPriorities["HUNTER-BM"][i] == "KINGS") then
-            kingsPriority = i
-            if (sanctuaryPriority and kingsPriority < sanctuaryPriority) then
-              WeakAuras.ScanEvent(customEvent..aura_env.defaultBuffPriorities["HUNTER-BM"][i])
+            if (sanctuaryAvailability and aura_env.defaultBuffPriorities[i][2][k] == "SANCTUARY") then
+              sanctuaryPriority = i
+              if (kingsPriority and sanctuaryPriority < kingsPriority) then
+                WeakAuras.ScanEvent(customEvent..aura_env.defaultBuffPriorities[i][2][k])
+              end
             end
-          end
-          if (sanctuaryAvailability and aura_env.defaultBuffPriorities["HUNTER-BM"][i] == "SANCTUARY") then
-            sanctuaryPriority = i
-            if (kingsPriority and sanctuaryPriority < kingsPriority) then
-              WeakAuras.ScanEvent(customEvent..aura_env.defaultBuffPriorities["HUNTER-BM"][i])
-            end
-          end
-        else
-          WeakAuras.ScanEvent(customEvent..aura_env.defaultBuffPriorities["HUNTER-BM"][i])
+          else
+            WeakAuras.ScanEvent(customEvent..aura_env.defaultBuffPriorities[i][2][k])
+          end  
         end
       end
     end
@@ -175,3 +154,4 @@ end
 
 -- Send custom event when buffs are about to run out (customizable in seconds!).
 -- check druid tank spec via if def = crit immune
+-- track fire elemental from shaman and more
